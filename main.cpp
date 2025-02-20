264abbcbb4485c9bc3b26e06d699273fa070577c
Asystole-2 <153837128+Asystole-2@users.noreply.github.com>
2025-02-20 13:19:16 +0000

    LegoSet struct made data from the csv file displaying

--- main.cpp
+++ main.cpp
@@ -1,25 +1,102 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iomanip>
+using namespace std;
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void readfile();
+
+void legosets();
+
+void display();
+
+void parse();
+
+/*
+ Formate: Set_ID,Set_Name,Theme,Piece_Count,Price
+
+Themes: Ninjago
+Marvel   Star Wars   Minecraft   Creator   City    Friends   Harry Potter
+Technic   Architecture   DC   Ideas   Super Mario   Disney   Jurassic World
+*/
+//std::vector<std::tuple <int, std::string> > myArr;
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
-
+    legosets();
     return 0;
 }
 
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
+struct LegoSet {
+    string setID;
+    string setName;
+    string theme;
+    int pieceCount;
+    double price;
+};
+
+void parse(string line, LegoSet &lego_set) {
+    stringstream ss(line);
+    string temp;
+
+    getline(ss, temp, ',');
+    lego_set.setID = temp;
+
+    getline(ss, temp, ',');
+    lego_set.setName = temp;
+
+    getline(ss, temp, ',');
+    lego_set.theme = temp;
+
+    getline(ss, temp, ',');
+    lego_set.pieceCount = stoi(temp);
+
+    getline(ss, temp, ',');
+    lego_set.price = stod(temp);
+}
+
+void readfile(string filename, vector<LegoSet> &legosets) {
+    LegoSet legoset;
+    ifstream fin(filename);
+    if (fin) {
+        string line;
+
+        while (!fin.eof()) {
+
+            getline(fin, line);
+            if (line!="") {
+                parse(line, legoset);
+                legosets.push_back(legoset);
+            }
+        }
+        fin.close();
+    } else {
+        cout << "Unable to open file" << endl;
+        exit(1);
+    }
+}
+
+void display(const vector<LegoSet> &legosets) {
+
+    cout << "--------------------------------------------------------\n";
+    cout << "                   Lego Collection  \n";
+    cout << "--------------------------------------------------------\n";
+
+    for (const auto &set : legosets) {
+        cout << " ID: " << set.setID << "\n";
+        cout << " Name: " << set.setName << "\n";
+        cout << " Theme: " << set.theme << "\n";
+        cout << " Pieces: " << set.pieceCount << "\n";
+        cout << fixed << std::setprecision(2);
+        cout << " Price: " << set.price << "\n";
+        cout << "--------------------------------------------------------\n";
+    }
+    cout << " End of Collection \n";
+}
+
+void legosets() {
+    vector<LegoSet> legosets;
+    string filename = "lego_sets_120.csv";
+    readfile(filename, legosets);
+    display(legosets);
+}
