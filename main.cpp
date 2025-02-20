11e4781d2f7625976cd7419341bb088e2f8f6046
Asystole-2 <153837128+Asystole-2@users.noreply.github.com>
2025-02-20 22:32:12 +0000

    function for finding index of entered field in vector of structs; use of function to find and display the contents of structs of the index

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <fstream>
 #include <iomanip>
+#include <set>
 using namespace std;
 
 void readfile();
@@ -14,6 +15,10 @@ void display();
 
 void parse();
 
+int indexofvalue();
+
+void displayLegomatchingSet_ID();
+
 /*
  Formate: Set_ID,Set_Name,Theme,Piece_Count,Price
 
@@ -22,6 +27,16 @@ Marvel   Star Wars   Minecraft   Creator   City    Friends   Harry Potter
 Technic   Architecture   DC   Ideas   Super Mario   Disney   Jurassic World
 */
 //std::vector<std::tuple <int, std::string> > myArr;
+
+/*2.	Write a function that will accept two parameters: the vector of structs and a value for a particular field (of your choosing).  The function will search through the vector elements for the value (in a particular field) and will return the index (int) of the located element,
+or -1 if no match is found.  Use the returned index to access and display the contents of the struct, otherwise, display a not found message.
+
+3.	Write a function that will accept the vector of structs,
+iterate through the data loaded in stage 2 and accumulate a count of the number of rows that are a match for each of the unique values in a chosen column.
+For example, if you chose movie data then this function could return the number of movies of each genre (comedy, thriller, drama, etc.).
+This function should return a map containing all information.
+*/
+
 int main() {
     legosets();
     return 0;
@@ -62,9 +77,8 @@ void readfile(string filename, vector<LegoSet> &legosets) {
         string line;
 
         while (!fin.eof()) {
-
             getline(fin, line);
-            if (line!="") {
+            if (line != "") {
                 parse(line, legoset);
                 legosets.push_back(legoset);
             }
@@ -77,12 +91,11 @@ void readfile(string filename, vector<LegoSet> &legosets) {
 }
 
 void display(const vector<LegoSet> &legosets) {
-
     cout << "--------------------------------------------------------\n";
     cout << "                   Lego Collection  \n";
     cout << "--------------------------------------------------------\n";
 
-    for (const auto &set : legosets) {
+    for (const auto &set: legosets) {
         cout << " ID: " << set.setID << "\n";
         cout << " Name: " << set.setName << "\n";
         cout << " Theme: " << set.theme << "\n";
@@ -93,10 +106,55 @@ void display(const vector<LegoSet> &legosets) {
     }
     cout << " End of Collection \n";
 }
+int indexofvalue(vector<LegoSet> &lego_sets, string &Set_ID) {
+    int index = -1;
+
+    if (!lego_sets.empty()) {
+        for (int i = 0; i < lego_sets.size(); i++) {
+            if (lego_sets[i].setID == Set_ID) {
+                index = i;
+            }
+        }
+    } else {
+        cout << " The vector is empty " << endl;
+    }
+
+    return index;
+}
+
+void displayLegomatchingSet_ID(vector<LegoSet> &lego_sets) {
+    string legoSet_ID;
+    cout << "Enter the ID for the lego set you are looking for: " << endl;
+    cin >> legoSet_ID;
+    int index = indexofvalue(lego_sets, legoSet_ID);
+
+    cout << "--------------------------------------------------------\n";
+    cout << "            Search result for: " << legoSet_ID << endl;
+    cout << "--------------------------------------------------------\n";
+
+    if (index > -1 && index <= lego_sets.size()) {
+        if (!lego_sets.empty()) {
+            cout << " Name: " << lego_sets[index].setName << "\n";
+            cout << " Theme: " << lego_sets[index].theme << "\n";
+            cout << " Piece count: " <<lego_sets[index].pieceCount << "\n";
+            cout << " Price: " << lego_sets[index].price << " euro\n";
+            cout << "--------------------------------------------------------\n";
+        }
+        else {
+            cout << " The vector is empty " << endl;
+        }
+    } else {
+        cout << "Invalid set ID [index: " << index <<"]" << endl;
+    }
+
+}
 
 void legosets() {
     vector<LegoSet> legosets;
     string filename = "lego_sets_120.csv";
     readfile(filename, legosets);
-    display(legosets);
+    // display(legosets);
+    displayLegomatchingSet_ID(legosets);
 }
+
+
