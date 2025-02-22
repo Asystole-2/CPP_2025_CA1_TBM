543bb57615dfd9fd18ab106deed1ada5bcb1a2eb
Asystole-2 <153837128+Asystole-2@users.noreply.github.com>
2025-02-22 17:52:10 +0000

    function for finding highest, lowest and average piece count; better naming for my variables; refinements

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <iomanip>
 #include <limits>
 #include <map>
-#include <set>
+
 using namespace std;
 
 void readfile();
@@ -25,32 +25,19 @@ void displayLegoMatchingTheme();
 
 void displayLegoMatchingName();
 
+void pieceCountInformation();
+
 void menu();
-
-/*
- Formate: Set_ID,Set_Name,Theme,Piece_Count,Price
-
-Themes: Ninjago
-Marvel   Star Wars   Minecraft   Creator   City    Friends   Harry Potter
-Technic   Architecture   DC   Ideas   Super Mario   Disney   Jurassic World
-*/
-//std::vector<std::tuple <int, std::string> > myArr;
-/*
-3.	Write a function that will accept the vector of structs,
-iterate through the data loaded in stage 2 and accumulate a count of the number of rows that are a match for each of the unique values in a chosen column.
-For example, if you chose movie data then this function could return the number of movies of each genre (comedy, thriller, drama, etc.).
-This function should return a map containing all information.
-*/
 
 struct LegoSet {
     string setID;
     string setName;
     string theme;
-    int pieceCount;
-    double price;
+    int pieceCount = 0;
+    double price = 0;
 };
 
-void parse(string line, LegoSet &lego_set) {
+void parse(const string& line, LegoSet &lego_set) {
     stringstream ss(line);
     string temp;
 
@@ -70,17 +57,16 @@ void parse(string line, LegoSet &lego_set) {
     lego_set.price = stod(temp);
 }
 
-void readfile(string filename, vector<LegoSet> &legosets) {
-    LegoSet legoset;
-    ifstream fin(filename);
-    if (fin) {
+void readfile(const string& filename, vector<LegoSet> &legoSets) {
+    if (ifstream fin(filename); fin) {
+        LegoSet legoSet;
         string line;
 
         while (!fin.eof()) {
             getline(fin, line);
-            if (line != "") {
-                parse(line, legoset);
-                legosets.push_back(legoset);
+            if (!line.empty()) {
+                parse(line, legoSet);
+                legoSets.push_back(legoSet);
             }
         }
         fin.close();
@@ -90,12 +76,12 @@ void readfile(string filename, vector<LegoSet> &legosets) {
     }
 }
 
-void display(const vector<LegoSet> &legosets) {
+void display(const vector<LegoSet> &legoSets) {
     cout << "--------------------------------------------------------\n";
     cout << "|                   LEGO COLLECTION                    |\n";
     cout << "--------------------------------------------------------\n";
 
-    for (const auto &set: legosets) {
+    for (const auto &set: legoSets) {
         cout << " ID: " << set.setID << "\n";
         cout << " Name: " << set.setName << "\n";
         cout << " Theme: " << set.theme << "\n";
@@ -107,10 +93,10 @@ void display(const vector<LegoSet> &legosets) {
     cout << " End of Collection \n";
 }
 
-map<string, int> themeMap(const vector<LegoSet> & legosets);
+map<string, int> themeMap(const vector<LegoSet> & legoSets);
 
-void displayThemeMap(const vector<LegoSet> &legosets){
-    map <string, int> themes = themeMap(legosets);
+void displayThemeMap(const vector<LegoSet> &legoSets){
+    map <string, int> themes = themeMap(legoSets);
 
     cout << "<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>> "<< endl;
     cout << "<              THEMES              >" << endl;
@@ -122,15 +108,16 @@ void displayThemeMap(const vector<LegoSet> &legosets){
     cout << "<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>> "<< endl;
 
 }
- map<string,int> themeMap(const vector<LegoSet> &legosets) {
+
+ map<string,int> themeMap(const vector<LegoSet> &legoSets) {
     map<string,int > themeCount;
-    for (const auto &set: legosets) {
+    for (const auto &set: legoSets) {
         themeCount[set.theme]++;
     }
     return themeCount;
 }
 
-int indexOfName(const vector<LegoSet> &lego_sets, string &setName) {
+int indexOfName(const vector<LegoSet> &lego_sets, const string &setName) {
     int index = -1;
 
     if (!lego_sets.empty()) {
@@ -178,18 +165,18 @@ void displayLegoMatchingTheme(const vector<LegoSet> &lego_sets) {
 }
 
 void displayLegoMatchingName(const vector<LegoSet> &lego_sets) {
-    string legoSetname;
+    string legoSetName;
     cout << "* * * * * * * * * * * * * * * * * *" << endl;
     cout << "*           NAME  SEARCH          *" << endl;
     cout << "* * * * * * * * * * * * * * * * * *" << endl;
     cout << "Enter the name for the lego set you are looking for: " << endl;
     cin.ignore();
-    getline(cin, legoSetname);
-    int index = indexOfName(lego_sets, legoSetname);
+    getline(cin, legoSetName);
+    int index = indexOfName(lego_sets, legoSetName);
 
     cout << "-------------------------------------\n";
     cout << "         Search result for: " << endl;
-    cout << "         " << legoSetname << endl;
+    cout << "         " << legoSetName << endl;
     cout << "-------------------------------------\n";
 
     if (index > -1 && index <= lego_sets.size()) {
@@ -207,7 +194,59 @@ void displayLegoMatchingName(const vector<LegoSet> &lego_sets) {
     }
 }
 
-void menu(const vector<LegoSet> &legosets) {
+void pieceCountInformation(const vector<LegoSet> &lego_sets) {
+    int mostPieces = 0, leastPieces = mostPieces, allPieces = 0 ;
+
+    for (const auto & lego_set : lego_sets) {
+
+        allPieces += lego_set.pieceCount;
+
+        if (lego_set.pieceCount > mostPieces) {
+            mostPieces = lego_set.pieceCount;
+        }
+        if (lego_set.pieceCount < mostPieces) {
+            leastPieces = lego_set.pieceCount;
+        }
+    }
+    cout << "###########################################################\n";
+    cout << "##                 SET PIECE INFORMATICS                 ##" << endl;
+    cout << "###########################################################\n\n";
+
+    for (const auto & lego_set : lego_sets) {
+        if (lego_set.pieceCount == leastPieces) {
+            cout << "           ----------------------------------------"<< endl;
+            cout << "            Lego set with the lowest piece-count: "<< endl;
+            cout << "           ----------------------------------------"<< endl;
+            cout << "                  " << lego_set.setName << "\n";
+            cout << "                  " << lego_set.theme << "\n";
+            cout << "                  " << lego_set.price << " euro\n";
+            cout << "           -----------------------------------------"<< endl;
+            cout << "            Contains:   * " <<leastPieces << " pieces *" << endl;
+            cout << "           ----------------------------------------"<< endl;
+        }
+        if (lego_set.pieceCount == mostPieces) {
+            cout << "           ----------------------------------------"<< endl;
+            cout << "            Lego set with the highest piece-count:"<< endl;
+            cout << "           ----------------------------------------"<< endl;
+            cout << "                " << lego_set.setName << "\n";
+            cout << "                " << lego_set.theme << "\n";
+            cout << "                " << lego_set.price << " euro\n";
+            cout << "           ---------------------------------------"<< endl;
+            cout << "            Contains:   * " <<mostPieces << " pieces *" << endl;
+            cout << "           ----------------------------------------"<< endl;
+            cout << "  ...........................SPI..........................\n";
+
+        }
+    }
+
+    const int averagePieces = allPieces / lego_sets.size();
+    cout << "\n  Average piece count of all sets in the collection: " << averagePieces << "\n";
+    cout << "\n###########################################################\n";
+
+}
+
+
+void menu(const vector<LegoSet> &legoSets) {
     int choice;
     do {
         cout << "--------------------------------------------------\n";
@@ -217,7 +256,8 @@ void menu(const vector<LegoSet> &legosets) {
         cout << "         2. Search for a Lego set by name\n";
         cout << "         3. Theme Map\n";
         cout << "         4. Search for a Lego set by theme\n";
-        cout << "         5. Exit\n";
+        cout << "         5. Piece Count Insights\n";
+        cout << "         6. Exit\n";
         cout << " Enter your choice: ";
 
         cin.clear();
@@ -232,31 +272,49 @@ void menu(const vector<LegoSet> &legosets) {
 
         switch (choice) {
             case 1:
-                display(legosets);
+                display(legoSets);
                 break;
             case 2:
-                displayLegoMatchingName(legosets);
+                displayLegoMatchingName(legoSets);
                 break;
             case 3:
-                displayThemeMap(legosets);
+                displayThemeMap(legoSets);
             break;
             case 4:
-                displayLegoMatchingTheme(legosets);
+                displayLegoMatchingTheme(legoSets);
                 break;
             case 5:
-                cout << "Exiting the program.\n";
+                pieceCountInformation(legoSets);
                 break;
+            case 6:
+                cout << "Exiting the program.\n";
+            break;
             default:
                 cout << "Invalid choice. Please try again.\n";
             break;
         }
-    } while (choice != 5);
+    } while (choice != 6);
 }
 
 int main() {
-    vector<LegoSet> legosets;
-    string filename = "lego_sets_120.csv";
-    readfile(filename, legosets);
-    menu(legosets);
+    vector<LegoSet> legoSets;
+    const string filename = "lego_sets_120.csv";
+    readfile(filename, legoSets);
+    menu(legoSets);
     return 0;
 }
+
+/*
+ Total amount of pieces:252,702
+  Highest: 5923
+  Lowest: 830
+  numbers = [
+2420, 4708, 4062, 378, 2016, 143, 4535, 1969, 3347, 4726, 1186, 2935, 1050, 4911, 1949,
+332, 1999, 923, 4581, 1731, 2259, 3636, 2554, 2962, 4917, 1162, 2273, 4747, 2566, 1762,
+1873, 4713, 4911, 735, 3517, 3151, 4571, 2958, 170, 3606, 4050, 191, 2279, 3180, 1899,
+3271, 3074, 1174, 360, 2858, 1316, 4123, 3121, 2571, 749, 2803, 347, 4241, 951, 1970,
+838, 4112, 4951, 3518, 2568, 4533, 1804, 4953, 3202, 3858, 4941, 2635, 3355, 3056, 2381,
+2318, 4184, 3581, 572, 54, 3036, 3200, 480, 2207, 1360, 1677, 1070, 922, 1197, 2079,
+5923, 231, 2352, 3210, 878, 3120, 565, 2354, 1125, 830
+]
+*/
