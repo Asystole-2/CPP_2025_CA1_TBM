50747b80337e33d6dcfe5d475b3c9a44449240cd
Asystole-2 <153837128+Asystole-2@users.noreply.github.com>
2025-02-23 18:23:27 +0000

    csv file corrections; ascending and descending order for sorting and displaying the structs; used a comparator for the ascending order (https://www.geeksforgeeks.org/sort-c-stl/)

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -27,6 +28,10 @@ void displayLegoMatchingName();
 
 void pieceCountInformation();
 
+void displayDescFloat();
+
+void displayAscFloat();
+
 void menu();
 
 struct LegoSet {
@@ -34,10 +39,10 @@ struct LegoSet {
     string setName;
     string theme;
     int pieceCount = 0;
-    double price = 0;
+    double price = 0.0;
 };
 
-void parse(const string& line, LegoSet &lego_set) {
+void parse(const string &line, LegoSet &lego_set) {
     stringstream ss(line);
     string temp;
 
@@ -57,7 +62,7 @@ void parse(const string& line, LegoSet &lego_set) {
     lego_set.price = stod(temp);
 }
 
-void readfile(const string& filename, vector<LegoSet> &legoSets) {
+void readfile(const string &filename, vector<LegoSet> &legoSets) {
     if (ifstream fin(filename); fin) {
         LegoSet legoSet;
         string line;
@@ -93,24 +98,67 @@ void display(const vector<LegoSet> &legoSets) {
     cout << " End of Collection \n";
 }
 
-map<string, int> themeMap(const vector<LegoSet> & legoSets);
+void displayDescFloat(vector<LegoSet> &legoSets) {
+    auto func = [](LegoSet x, LegoSet y) { return x.price > y.price; };
+    sort(legoSets.begin(), legoSets.end(), func);
 
-void displayThemeMap(const vector<LegoSet> &legoSets){
-    map <string, int> themes = themeMap(legoSets);
+    cout << "--------------------------------------------------------\n";
+    cout << "|               HIGHEST TO LOWEST PRICE                 |\n";
+    cout << "--------------------------------------------------------\n";
 
-    cout << "<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>> "<< endl;
-    cout << "<              THEMES              >" << endl;
-    cout << "<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>> "<< endl;
-
-    for (const auto &[theme,count] : themes) {
-        cout <<"         > "<< theme <<": "<< count<< endl;
+    for (const auto &set: legoSets) {
+        cout << " ID: " << set.setID << "\n";
+        cout << " Name: " << set.setName << "\n";
+        cout << " Theme: " << set.theme << "\n";
+        cout << " Pieces: " << set.pieceCount << "\n";
+        cout << fixed << std::setprecision(2);
+        cout << " Price: " << set.price << "\n";
+        cout << "--------------------------------------------------------\n";
     }
-    cout << "<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>> "<< endl;
-
+    cout << " End of Collection \n";
 }
 
- map<string,int> themeMap(const vector<LegoSet> &legoSets) {
-    map<string,int > themeCount;
+bool customComparator(const LegoSet &a, const LegoSet &b) {
+    return a.price < b.price;
+}
+
+void displayAscFloat(vector<LegoSet> &legoSets) {
+    sort(legoSets.begin(), legoSets.end(), customComparator);
+
+    cout << "--------------------------------------------------------\n";
+    cout << "|               LOWEST TO HIGHEST PRICE                 |\n";
+    cout << "--------------------------------------------------------\n";
+
+    for (const auto &set: legoSets) {
+        cout << " ID: " << set.setID << "\n";
+        cout << " Name: " << set.setName << "\n";
+        cout << " Theme: " << set.theme << "\n";
+        cout << " Pieces: " << set.pieceCount << "\n";
+        cout << fixed << std::setprecision(2);
+        cout << " Price: " << set.price << "\n";
+        cout << "--------------------------------------------------------\n";
+    }
+    cout << " End of Collection \n";
+}
+
+
+map<string, int> themeMap(const vector<LegoSet> &legoSets);
+
+void displayThemeMap(const vector<LegoSet> &legoSets) {
+    map<string, int> themes = themeMap(legoSets);
+
+    cout << "<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>> " << endl;
+    cout << "<              THEMES              >" << endl;
+    cout << "<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>> " << endl;
+
+    for (const auto &[theme,count]: themes) {
+        cout << "         > " << theme << ": " << count << endl;
+    }
+    cout << "<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>> " << endl;
+}
+
+map<string, int> themeMap(const vector<LegoSet> &legoSets) {
+    map<string, int> themeCount;
     for (const auto &set: legoSets) {
         themeCount[set.theme]++;
     }
@@ -195,10 +243,9 @@ void displayLegoMatchingName(const vector<LegoSet> &lego_sets) {
 }
 
 void pieceCountInformation(const vector<LegoSet> &lego_sets) {
-    int mostPieces = 0, leastPieces = mostPieces, allPieces = 0 ;
+    int mostPieces = 0, leastPieces = mostPieces, allPieces = 0;
 
-    for (const auto & lego_set : lego_sets) {
-
+    for (const auto &lego_set: lego_sets) {
         allPieces += lego_set.pieceCount;
 
         if (lego_set.pieceCount > mostPieces) {
@@ -212,52 +259,52 @@ void pieceCountInformation(const vector<LegoSet> &lego_sets) {
     cout << "##                 SET PIECE INFORMATICS                 ##" << endl;
     cout << "###########################################################\n\n";
 
-    for (const auto & lego_set : lego_sets) {
+    for (const auto &lego_set: lego_sets) {
         if (lego_set.pieceCount == leastPieces) {
-            cout << "           ----------------------------------------"<< endl;
-            cout << "            Lego set with the lowest piece-count: "<< endl;
-            cout << "           ----------------------------------------"<< endl;
+            cout << "           ----------------------------------------" << endl;
+            cout << "            Lego set with the lowest piece-count: " << endl;
+            cout << "           ----------------------------------------" << endl;
             cout << "                  " << lego_set.setName << "\n";
             cout << "                  " << lego_set.theme << "\n";
             cout << "                  " << lego_set.price << " euro\n";
-            cout << "           -----------------------------------------"<< endl;
-            cout << "            Contains:   * " <<leastPieces << " pieces *" << endl;
-            cout << "           ----------------------------------------"<< endl;
+            cout << "           -----------------------------------------" << endl;
+            cout << "            Contains:   * " << leastPieces << " pieces *" << endl;
+            cout << "           ----------------------------------------" << endl;
         }
         if (lego_set.pieceCount == mostPieces) {
-            cout << "           ----------------------------------------"<< endl;
-            cout << "            Lego set with the highest piece-count:"<< endl;
-            cout << "           ----------------------------------------"<< endl;
+            cout << "           ----------------------------------------" << endl;
+            cout << "            Lego set with the highest piece-count:" << endl;
+            cout << "           ----------------------------------------" << endl;
             cout << "                " << lego_set.setName << "\n";
             cout << "                " << lego_set.theme << "\n";
             cout << "                " << lego_set.price << " euro\n";
-            cout << "           ---------------------------------------"<< endl;
-            cout << "            Contains:   * " <<mostPieces << " pieces *" << endl;
-            cout << "           ----------------------------------------"<< endl;
+            cout << "           ---------------------------------------" << endl;
+            cout << "            Contains:   * " << mostPieces << " pieces *" << endl;
+            cout << "           ----------------------------------------" << endl;
             cout << "  ...........................SPI..........................\n";
-
         }
     }
 
     const int averagePieces = allPieces / lego_sets.size();
     cout << "\n  Average piece count of all sets in the collection: " << averagePieces << "\n";
     cout << "\n###########################################################\n";
-
 }
 
-
-void menu(const vector<LegoSet> &legoSets) {
+void menu(vector<LegoSet> &legoSets) {
     int choice;
     do {
         cout << "--------------------------------------------------\n";
         cout << " *            LEGO COLLECTION MENU              * \n";
-        cout << "--------------------------------------------------\n";
+        cout << "--------------------------------------------------\n\n";
         cout << "         1. Display all Lego sets\n";
         cout << "         2. Search for a Lego set by name\n";
         cout << "         3. Theme Map\n";
         cout << "         4. Search for a Lego set by theme\n";
         cout << "         5. Piece Count Insights\n";
-        cout << "         6. Exit\n";
+        cout << "         6. Prices in ascending order\n";
+        cout << "         7. Prices in descending order\n";
+        cout << "         8. Exit\n";
+
         cout << " Enter your choice: ";
 
         cin.clear();
@@ -279,7 +326,7 @@ void menu(const vector<LegoSet> &legoSets) {
                 break;
             case 3:
                 displayThemeMap(legoSets);
-            break;
+                break;
             case 4:
                 displayLegoMatchingTheme(legoSets);
                 break;
@@ -287,13 +334,19 @@ void menu(const vector<LegoSet> &legoSets) {
                 pieceCountInformation(legoSets);
                 break;
             case 6:
+                displayAscFloat(legoSets);
+                break;
+            case 7:
+                displayDescFloat(legoSets);
+                break;
+            case 8:
                 cout << "Exiting the program.\n";
-            break;
+                break;
             default:
                 cout << "Invalid choice. Please try again.\n";
-            break;
+                break;
         }
-    } while (choice != 6);
+    } while (choice != 8);
 }
 
 int main() {
@@ -303,6 +356,13 @@ int main() {
     menu(legoSets);
     return 0;
 }
+
+/*
+6.	Write a function that will search through the data and return a list of all items that match or partially match a given text input.
+For example, all movies that contain the text “The” in the title. Iterators must be used for this function.
+
+*/
+
 
 /*
  Total amount of pieces:252,702
@@ -318,3 +378,5 @@ int main() {
 5923, 231, 2352, 3210, 878, 3120, 565, 2354, 1125, 830
 ]
 */
+
+//References:    https://www.geeksforgeeks.org/sort-c-stl/
